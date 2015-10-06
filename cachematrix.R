a4b77709156c093f55fe2aa3e43a83d7bcb9c246
Cindy <xinyuechen213@gmail.com>
2015-10-06 16:33:24 -0400

    assignment2 completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming
+## Week 3, Programming Assignment 2
 
-## Write a short comment describing this function
-
+# makeCacheMatrix: 
+# This function creates a special "matrix" object that can cache its inverse.
+# This function is really a list containing a function to
+# 1.set the value of the matrix
+# 2.get the value of the matrix
+# 3.set the value of the mean
+# 4.get the value of the mean
 makeCacheMatrix <- function(x = matrix()) {
-
+   # initialize the matrix's inverse to NULL
+   inv <- NULL
+   
+   # set the value of the matrix
+   set <- function(y) {
+           x <<- y
+           inv <<- NULL
+   }
+   
+   # get the value of the matrix
+   get <- function() x
+   
+   # set the value of the inverse
+   setinverse <- function(inverse) inv <<- inverse
+   
+   # get the value of the inverse
+   getinverse <- function() inv
+   
+   # return a list of the above functions
+   list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve: 
+# This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above. If the inverse has already been calculated (and the 
+# matrix has not changed), then the cachesolve should retrieve the inverse 
+# from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # check if the inverse is already cached,
+        # if so, we get the inverse from the cache directly
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        # else, we first get the matrix
+        data <- x$get()
+        # then calculate the inverse using solve function
+        inv <- solve(data, ...)
+        # then set the value of the matrix's inverse
+        x$setinverse(inv)
+        # return the result
+        inv
 }
